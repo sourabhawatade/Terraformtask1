228e7e6ca59b12a05bfa474351112997af57e98b
unknown <sourabhawatade12@gmail.com>
2025-07-18 11:46:48 +0530

    resource group added

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ terraform {
  backend "azurerm" {
    storage_account_name = "tflockstatefile"
    container_name = "tflock1"
-   resource_group_name = "tf-state"
+   resource_group_name = "ex-state"
    key = "terraform.tfstate"   
  }
 }
